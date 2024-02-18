f9401722ee2e22778daa2d41e6218da2c82892c0
Hemant-Kshirsagar22 <hemantkshirsagar23@gmail.com>
2024-02-18 16:40:41 +0530

    Patterns

---
+++ 06-LoopPatterns/08/pattern.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int numberOne = 0;
+int numberTwo = 0;
+int counterNumber = 0;
+int numberOfRows = 0;
+
+int main(void)
+{
+	printf("\n\nEnter Number Of Rows : ");
+	scanf("%d",&numberOfRows);
+
+	printf("\n\n");
+
+	counterNumber = 1;
+
+	for(numberOne = 1; numberOne <= numberOfRows;numberOne++)
+	{
+		for(numberTwo = numberOfRows; numberTwo >= numberOne;numberTwo--,counterNumber++)
+		{
+			printf(" %d ",counterNumber);
+		}
+
+		printf("\n");
+	}
+
+	printf("\n\n");
+
+	return(0);
+}
+
+/*
+OUTPUT
+
+
+Enter Number Of Rows : 4
+
+
+ 1  2  3  4
+ 5  6  7
+ 8  9
+ 10
+
+
+
+*/
---
+++ 06-LoopPatterns/10/pattern.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int numberOne = 0;
+int numberTwo = 0;
+int counterNumber = 0;
+int numberOfRows = 0;
+
+int main(void)
+{
+	printf("\n\nEnter Number Of Rows : ");
+	scanf("%d",&numberOfRows);
+
+	printf("\n\n");
+
+	counterNumber = 1;
+
+	for(numberOne = numberOfRows; numberOne >= 1;numberOne--)
+	{
+		for(numberTwo = numberOfRows ;numberTwo > numberOne;numberTwo--)
+		{
+			printf(" ");
+		}
+
+		for(numberTwo = 1; numberTwo <= numberOne;numberTwo++,counterNumber++)
+		{
+			printf("%d",counterNumber);
+		}
+
+		printf("\n");
+	}
+
+	printf("\n\n");
+
+	return(0);
+}
+
+/*
+OUTPUT
+
+
+Enter Number Of Rows : 4
+
+
+1234
+ 567
+  89
+   10
+
+
+
+*/

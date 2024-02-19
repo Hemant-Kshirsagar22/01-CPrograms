6cfcd280b09b8a8710c44fd7175a3c58dc94f7b8
Hemant-Kshirsagar22 <hemantkshirsagar23@gmail.com>
2024-02-19 16:15:01 +0530

    patterns

---
+++ 06-LoopPatterns/12/pattern.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int numberOne = 0;
+int numberTwo = 0;
+
+int main(void)
+{
+	for(numberOne = 0; numberOne <= 3; numberOne++)
+	{
+		for(numberTwo = 0; numberTwo <= 3; numberTwo++)
+		{
+			
+		}
+	}
+
+	return(0);
+}
